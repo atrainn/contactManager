d0ec81dcea3192e35b74d92da96ca87e2a22aa1f
delti90 <athomas90@gmail.com>
2014-07-31 16:52:09 -0400

    secondCommit

--- main/java/io/anthonythomas/contactmanager/MyActivity.java
+++ main/java/io/anthonythomas/contactmanager/MyActivity.java
@@ -7,10 +7,13 @@ import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TabHost;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,7 @@ public class MyActivity extends Activity {
 
     EditText nameTxt, phoneTxt, emailTxt, addressTxt;
     List<Contact> Contacts = new ArrayList<Contact>();
+    ListView contactListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +34,7 @@ public class MyActivity extends Activity {
         phoneTxt =(EditText) findViewById(R.id.txtPhone);
         emailTxt = (EditText) findViewById(R.id.txtEmail);
         addressTxt = (EditText) findViewById(R.id.txtAddress);
-
-
+        contactListView = (ListView) findViewById((R.id.listView));
         TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
         tabHost.setup();
 
@@ -51,8 +54,10 @@ public class MyActivity extends Activity {
        final Button addBtn = (Button) findViewById(R.id.btnAdd);
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Your contact has been created!", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                addContacts(nameTxt.getText().toString(), phoneTxt.getText().toString(), emailTxt.getText().toString(), addressTxt.getText().toString());
+                populateList();
+                Toast.makeText(getApplicationContext(), nameTxt.getText().toString() + " has been added to your Contacts!", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -74,10 +79,38 @@ public class MyActivity extends Activity {
         });
     }
 
+    private void populateList() {
+        ArrayAdapter<Contact> adapter = new ContactListAdapter();
+        contactListView.setAdapter(adapter);
+    }
+
+    private void addContacts(String name, String phone, String email, String address) {
+
+        Contacts.add(new Contact(name, phone, email, address));
+    }
 
     private class ContactListAdapter extends ArrayAdapter<Contact> {
         public ContactListAdapter() {
+
             super(MyActivity.this, R.layout.listview_item, Contacts);
+        }
+        @Override
+        public View getView(int position, View view, ViewGroup parent){
+            if (view == null)
+                view = getLayoutInflater().inflate(R.layout.listview_item, parent, false);
+
+            Contact currentContact = Contacts.get(position);
+
+            TextView name = (TextView) view.findViewById(R.id.contactName);
+            name.setText(currentContact.getName());
+            TextView phone = (TextView) view.findViewById(R.id.phoneNumber);
+            phone.setText(currentContact.getPhone());
+            TextView email = (TextView) view.findViewById(R.id.emailAddress);
+            phone.setText(currentContact.getEmail());
+            TextView address = (TextView) view.findViewById(R.id.cAddress);
+            phone.setText(currentContact.getAddress());
+
+            return view;
         }
     }
     @Override
